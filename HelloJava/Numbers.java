7281f0b338ff48d5b81aea9d5a5d38739e93c57f
watmonster <112777372+watmonster@users.noreply.github.com>
2022-09-03 12:47:29 -0700

    wip Numbers.java

---
+++ HelloJava/Numbers.java
@@ -0,0 +1,19 @@
+package HelloJava;
+
+//Write a program that uses the Scanner class to ask for numbers
+//from the user. After every number,
+//print out the total of all of the entered numbers,
+//as well as the min, max, and average.
+
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args){
+        int[] nums = {};
+
+        for(int i = 0; i < 3; i++){
+            
+        }
+
+    }
+}
