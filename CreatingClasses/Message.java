22890335e1fc5436ebe901e421366b9d5d47a11d
watmonster <112777372+watmonster@users.noreply.github.com>
2022-09-10 11:51:39 -0700

    added instancecount stuff

--- CreatingClasses/Message.java
+++ CreatingClasses/Message.java
@@ -1,17 +1,23 @@
 package CreatingClasses;
 
 public class Message {
-    
-    String myMessage;
+    static int instanceCount = 0;
+	
+	String myMessage;
 	
 	public Message(String myMessage){
 		this.myMessage = myMessage;
+		Message.instanceCount++;
 	}
 	
 	public void printMessage(){
 		System.out.println(myMessage);
 	}
-		
+	
+	public static void printInstanceCount(){
+		System.out.println("There are " + Message.instanceCount + " instances of Message.");
+	}
+	
 	public static void main(String[] args){
 		Message messageOne = new Message("Hello");
 		Message messageTwo = new Message("Bonjour");
@@ -20,5 +26,7 @@ public class Message {
 		messageOne.printMessage(); //prints Hello
 		messageTwo.printMessage(); //prints Bonjour
 		messageThree.printMessage(); //prints Hola
+
+		Message.printInstanceCount(); //prints 3
 	}
 }
