e2c49eda9dfbec874fb8d7913c051929d7ffc6a0
watmonster <112777372+watmonster@users.noreply.github.com>
2022-09-17 11:57:59 -0700

    WIP errors unit hw

--- ErrorsUnit/RandPoints.java
+++ ErrorsUnit/RandPoints.java
@@ -6,21 +6,45 @@ import java.util.Random;
 
 public class RandPoints {
     
-    public static void main(String[] args){
+    private ArrayList<Point> points;
+
+    
+
+    // public static void writePointsToFile(ArrayList<Point> points){
+    //     //somestuff
+    // }
+
+    public RandPoints(ArrayList<Point> points){
+        this.points = points;
+    }
+
+    // public Point center(){
+    //     //TODO
+    // }
+
+    // public void writeFile(String filePath){
+    //     //TODO
+    // }
+
+    // public static RandPoints readFile(String filePath){
+    //     //TODO
+    // }
+
+    public static RandPoints makePoints(int numPoints){
         //makes some points
         Random rand = new Random();
         ArrayList<Point> points = new ArrayList<Point>();
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < numPoints; i++){
             int x = rand.nextInt(20);
             int y = rand.nextInt(20);
             points.add(new Point(x,y));
         }
-        System.out.println(points);
-        //calls a function to put them in a text file
+        return new RandPoints(points);
     }
 
-    public static void writePointsToFile(ArrayList<Point> points){
-        //somestuff
+    public void printPoints(){
+        System.out.println("Here are your points: ");
+        System.out.println(this.points);
     }
 
 }
---
+++ ErrorsUnit/RandPointsMain.java
@@ -0,0 +1,10 @@
+package ErrorsUnit;
+
+public class RandPointsMain {
+    
+    public static void main(String[] args){
+        RandPoints myPoints = RandPoints.makePoints(20);
+        myPoints.printPoints();
+    }
+
+}
