1479d3e600bbf5365c047888a9e47c4a77701163
watmonster <112777372+watmonster@users.noreply.github.com>
2022-10-02 16:07:19 -0700

    we got it working maybe?

--- ProcessingUnit/MySketch.java
+++ ProcessingUnit/MySketch.java
@@ -2,8 +2,19 @@ package ProcessingUnit;
 
 import processing.core.PApplet;
 
-public class MySketch {
-    public static void main(String[] args){
-        System.out.println("hello");
+public class MySketch extends PApplet {
+
+	public void settings() {
+		size(500, 500);
+	}
+
+	public void draw(){
+		background(64);
+		ellipse(mouseX, mouseY, 20, 20);
+	}
+  
+	public static void main(String[] passedArgs) {
+		String[] appletArgs = new String[] { "MySketch" };
+		PApplet.main(appletArgs);
     }
 }
